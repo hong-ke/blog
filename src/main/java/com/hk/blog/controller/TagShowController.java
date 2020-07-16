2db98e78533319c33cd808caeb3301624333ae44
hong-ke <893312233@qq.com>
2020-07-16 09:56:12 +0800

    更新了标签模块

--- src/main/java/com/hk/blog/controller/TagShowController.java
+++ src/main/java/com/hk/blog/controller/TagShowController.java
@@ -2,6 +2,9 @@ package com.hk.blog.controller;
 
 import com.hk.blog.service.BlogService;
 import com.hk.blog.service.TagService;
+import com.hk.blog.vo.BlogQuery;
+import com.hk.blog.vo.TagTopVO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,16 +23,15 @@ public class TagShowController {
     @Autowired
     private BlogService blogService;
 
-   /* @GetMapping("/tags/{id}")
-    public String tags(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
-                       @PathVariable Long id, Model model) {
-        List<Tag> tags = tagService.listTagTop(10000);
+    @GetMapping("/tags/{id}")
+    public String tags(@Param("page") Integer page, BlogQuery blogQuery, @PathVariable Long id, Model model) {
+        List<TagTopVO> tags = tagService.findTop(10000);
         if (id == -1) {
            id = tags.get(0).getId();
         }
         model.addAttribute("tags", tags);
-        model.addAttribute("page", blogService.listBlog(id,pageable));
+        model.addAttribute("page", blogService.listBlog(page, blogQuery));
         model.addAttribute("activeTagId", id);
         return "tags";
-    }*/
+    }
 }
